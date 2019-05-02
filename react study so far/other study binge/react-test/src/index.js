eb4754af8c76e7acbbe3c4bea5c65acce75923e1
Tony-Chau <tonychau923@gmail.com>
2019-05-02 12:24:18 +1000

    comments

--- react study so far/other study binge/react-test/src/index.js
+++ react study so far/other study binge/react-test/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Due to JSX, we can write HTMl code within javascript
 const element = <h1> Hello World </h1>;
+//The DOM.render(name of the element/code and the id of an element where the code will be in)
 ReactDOM.render(element, document.getElementById('root'));
