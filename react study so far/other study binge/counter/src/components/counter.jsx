1c857e07e061184c28b02efbc400092f9694f337
Tony-Chau <tonychau923@gmail.com>
2019-05-04 13:31:17 +1000

    more tutorials stuff with comments

--- react study so far/other study binge/counter/src/components/counter.jsx
+++ react study so far/other study binge/counter/src/components/counter.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  //This is basically your global variable
+  state = {
+    count: 0,
+    address: {
+      street: ""
+    }
+  };
   render() {
     return (
       //React.Fragment is like Div, but allows the component inside to group within the destinated div
+      //Using {} within the HTML code, u can call functions, prop or state within to get the value you want
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    // You can also take a global variable like this
+    const { count } = this.state;
+    //You can also return a JSX expression or you could do
+    const green = <h1>Zero</h1>;
+    return count === 0 ? green : count;
   }
 }
 
