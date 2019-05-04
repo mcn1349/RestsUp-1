4d7c19c97d5230399f108f7ee1da561e805e35d8
Tony-Chau <tonychau923@gmail.com>
2019-05-04 14:26:25 +1000

    settings for class or variables in

--- react study so far/other study binge/counter/src/components/counter.jsx
+++ react study so far/other study binge/counter/src/components/counter.jsx
@@ -5,23 +5,23 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl:
-      "https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    address: {
-      street: ""
-    }
+      "https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png"
   };
   //You can add css stuff here, but its better to use the one You use custom made
   styles = {
-    fontSize: 10,
+    fontSize: 30,
     fontWeight: "bold"
   };
   render() {
+    //You can highlight some methods and refractor it with ctrl+shift+r in vsstudio
+    let classes = this.getBadgeClasses();
+
     return (
       //React.Fragment is like Div, but allows the component inside to group within the destinated div
       //Using {} within the HTML code, u can call functions, prop or state within to get the value you want
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={classes}>
           {this.formatCount()}
         </span>
         {/* it is optional to use style like this as well <span style={{fontSize: 15}}></span> */}
@@ -29,6 +29,14 @@ class Counter extends Component {
       </React.Fragment>
     );
   }
+  //This is like another global variable
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    //this sets just analyzes the class if the number is greater than 0 or not.
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     // You can also take a global variable like this
     const { count } = this.state;
